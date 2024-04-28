0a47b8c99757c9fca3d4e1b6c8a5c86057a2b816
domivbhh <hariharanvbit123@gmail.com>
2024-04-28 20:54:20 +0530

    .env removed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import {app,server} from './socket/socket.js';
+import path from 'path'
 
 
 import authRoutes from './routes/auth.routes.js'
@@ -13,6 +14,14 @@ import connectToDB from './db/connectToDB.js';
 
 //middleware
 const PORT=process.env.PORT || 5000;
+
+const __dirname=path.resolve();
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
+})
+
 app.use(cookieParser())
 
 dotenv.config()
