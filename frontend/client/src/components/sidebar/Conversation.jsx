9c0ab756a16db214c5f7e9b84f2389f163c18551
domivbhh <hariharanvbit123@gmail.com>
2024-04-28 17:18:18 +0530

    send message functionality completed

--- frontend/client/src/components/sidebar/Conversation.jsx
+++ frontend/client/src/components/sidebar/Conversation.jsx
@@ -50,7 +50,7 @@ import useConversation from '../../zustand/useConversation'
 
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <p className="font-bold text-gray-200">{conversation.fullname}</p>
             <span className="text-xl">{emoji}</span>
           </div>
         </div>
