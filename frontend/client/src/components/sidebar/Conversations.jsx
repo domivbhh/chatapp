b195740201ae2d3c40b1b116ccc382b3c239ba53
domivbhh <hariharanvbit123@gmail.com>
2024-04-28 16:19:16 +0530

    conversation sidebar ui created

--- frontend/client/src/components/sidebar/Conversations.jsx
+++ frontend/client/src/components/sidebar/Conversations.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Conversation from './Conversation';
+import useGetConversation from '../../hooks/useGetConversations';
+import { getRandomEmoji } from '../../utils/emojis';
 
 const Conversations = () => {
+  const{loading,conversations}=useGetConversation()
+  console.log(conversations)
   return (
     <div className="py-2 flex flex-col overflow-auto">
-       <Conversation />
-       <Conversation />
-       <Conversation />
-       <Conversation />
-       <Conversation />
-       <Conversation />
+      {
+        conversations.map((item,idx)=>(
+          <Conversation key={item.id} conversation={item} emoji={getRandomEmoji()} lastIdx={idx===conversations.length-1}/>
+        ))
+      }
+      {loading ? <span className='loading loading-spinner mx-auto'></span>:null}
       
     </div>
   );
